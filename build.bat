d1e06af52a910d98ab301960b76ef9d219680edd
Guilherme Martins <guilherme.rocha.martins@usp.br>
2021-07-31 18:05:12 -0300

    Update batch files and readme

--- build.bat
+++ build.bat
@@ -4,16 +4,13 @@
 color 06
 
 :: Window title
-title CMake setup for GiraffeMoor
+title Build GiraffeMoor 
 
-:: Create directory to build the solution and projects
-md build & cd .\build
+:: Go to build directory
+cd .\build
 
-:: Run CMake:
-cmake .. -DCMAKE_CONFIGURATION_TYPES="Debug;Release" -DMKL_INTERFACE=lp64
+:: Build solution
+cmake --build .
 
-:: Ask if should open the solution file
-set /p inputOpt= "Type [Y] and hit enter to open Visual Studio solution, or just hit enter to finish "
-if /i "%inputOpt:~,1%" EQU "y" start .\GiraffeMoor.sln 
 
 exit
--- open_VS.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-.\build\GiraffeMoor.sln 
-
-exit
